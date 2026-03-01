d51557c0f4e0e0d7e9222877fcb0c4e29646f080
Butschek <sirrroyale@gmail.com>
2026-03-01 22:35:54 +0100

    Remove Story Mode and use production API
    Remove the Story Mode menu button from index.html and update API_BASE in script.js to point to the deployed https://blutzucker-cfad.onrender.com endpoint instead of http://localhost:3000. This makes the UI arcade-only and switches the app to the production backend.

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const API_BASE = "http://localhost:3000";
+const API_BASE = "https://blutzucker-cfad.onrender.com";
 (function () { // Wrap in IIFE to avoid global scope pollution
   // --- Constants & Config ---
   const CONFIG = {
